bb480a5ec4a7cfb5b6ebbde52431763a14d1fdf2
kris <example@gmail.com>
2025-02-24 01:35:15 +0530

    module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeOrmConfig } from 'ormconfig';
+import { AuthModule } from './auth/auth.module';
+import { LinksModule } from './links/links.module';
+import { UsersModule } from './users/users.module';
+import { ClicksModule } from './clicks/clicks.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
+    ClicksModule,
+    LinksModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
